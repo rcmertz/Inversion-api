94ddbb2b1762d0dd35af085145013ce47a70127e
Vinícius Frasson <vinifrasson13@gmail.com>
2023-03-06 21:35:02 -0300

    MVC

---
+++ inversion/src/main/java/uniamerica/com/inversion/entity/AbstractEntity.java
@@ -0,0 +1,44 @@
+package uniamerica.com.inversion.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Getter @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Getter @Setter
+    @Column(name = "cadastro", nullable = false)
+    private LocalDateTime cadastro;
+
+    @Getter @Setter
+    @Column(name = "atualizado")
+    private LocalDateTime atualizado;
+
+    @Getter @Setter
+    @Column(name = "excluido")
+    private LocalDateTime excluido;
+
+    @Getter @Setter
+    @Column(name = "ativo", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private boolean ativo;
+
+    @PrePersist
+    public void dataCadastro(){
+        this.cadastro = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void dataAtualizacao(){
+        this.atualizado = LocalDateTime.now();
+    }
+
+}
---
+++ inversion/src/main/java/uniamerica/com/inversion/entity/Usuario.java
@@ -0,0 +1,46 @@
+package uniamerica.com.inversion.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@NoArgsConstructor
+@ToString
+@Table(name = "usuarios", schema = "public")
+public class Usuario extends AbstractEntity{
+
+    @Getter @Setter
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+    @Getter @Setter
+    @CPF
+    @Column(name = "cpf", unique = true, length = 15)
+    private String cpf;
+
+    @Getter @Setter
+    @Column(name = "telefone", length = 18)
+    private String telefone;
+
+    @Getter @Setter
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Getter @Setter
+    @Column(name = "senha", length = 50)
+    private String senha;
+
+    public Usuario(String nome, String cpf, String telefone, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+}
