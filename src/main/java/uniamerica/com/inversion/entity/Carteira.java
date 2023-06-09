2bd9e6744b13fad6d7095a700957ebfc2e5512b2
rcmertz <rcliberalli@hotmail.com>
2023-06-09 13:25:38 -0300

    Relacionamentos

--- src/main/java/uniamerica/com/inversion/entity/Carteira.java
+++ src/main/java/uniamerica/com/inversion/entity/Carteira.java
@@ -1,8 +1,6 @@
 package uniamerica.com.inversion.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
--- src/main/java/uniamerica/com/inversion/entity/Rendimento.java
+++ src/main/java/uniamerica/com/inversion/entity/Rendimento.java
@@ -1,8 +1,6 @@
 package uniamerica.com.inversion.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -36,10 +34,16 @@ public class Rendimento extends AbstractEntity {
     @Column(name = "descricao")
     private String descricao;
 
-    public Rendimento(Double preco_un, Double quantidade, LocalDate data, String descricao) {
+    @Getter @Setter
+    @JoinColumn(name= "idPapel")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Papel papel;
+
+    public Rendimento(Double preco_un, Double quantidade, LocalDate data, String descricao, Papel papel) {
         this.preco_un = preco_un;
         this.quantidade = quantidade;
         this.data = data;
         this.descricao = descricao;
+        this.papel = papel;
     }
 }
